06378bc209695ffef8d378614af0e6360e7bdfb5
Your Name <you@example.com>
2025-02-28 18:27:58 +0700

    add before_action callback in sleep records controller to set @user

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -1,13 +1,23 @@
 class SleepRecordsController < ApplicationController
+  before_action :set_user
+
   def index
     
   end
 
   def clock_in
-    puts params
+
   end
   
   def clock_out
     
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
   end
 end
