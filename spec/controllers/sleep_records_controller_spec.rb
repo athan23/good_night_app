8ba6499e90dbc98b615c82201329e08f7d54b1a5
Your Name <you@example.com>
2025-03-01 03:00:05 +0700

    add GET request for sleep records index controller. Sleep records are ordered by created_at

--- spec/controllers/sleep_records_controller_spec.rb
+++ spec/controllers/sleep_records_controller_spec.rb
@@ -23,4 +23,38 @@ RSpec.describe SleepRecordsController, type: :controller do
       end
     end
   end
+
+  describe "POST #clock_out" do
+    context "when the user has an active sleep record" do
+      let!(:record) { create(:sleep_record, user: user) }
+
+      it "updates the sleep record with clock_out time" do
+        post :clock_out, params: { user_id: user.id }
+        record.reload
+
+        expect(record.clock_out).not_to be_nil
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when the user has no active sleep record" do
+      it "returns an error" do
+        post :clock_out, params: { user_id: user.id }
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+  end
+
+  describe "GET #index" do
+    it "returns all sleep records of a user" do
+      older_record = create(:sleep_record, user: user, created_at: 2.days.ago)
+      newer_record = create(:sleep_record, user: user, created_at: 1.day.ago)
+
+      get :index, params: { user_id: user.id }
+
+      json_response = JSON.parse(response.body)
+      expect(json_response.first["id"]).to eq(older_record.id)
+      expect(json_response.last["id"]).to eq(newer_record.id)
+    end
+  end
 end
