0ccb8edf7c29e032844b18a06dcbea0cbda47253
Your Name <you@example.com>
2025-02-28 17:54:31 +0700

    add base sleep records controller which has index, clock in, and clock out operations

---
+++ app/controllers/sleep_records_controller.rb
@@ -0,0 +1,13 @@
+class SleepRecordsController < ApplicationController
+  def index
+    
+  end
+
+  def clock_in
+    
+  end
+  
+  def clock_out
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :sleep_records, only: [:index] do
+    post "clock_in", on: :collection
+    post "clock_out", on: :collection
+  end
 end
