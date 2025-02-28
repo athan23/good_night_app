d278d9fc76df707687c552f97a27196a106e9889
Your Name <you@example.com>
2025-03-01 03:57:48 +0700

    add the followers and following relationship in the User and Follow classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
+
+  has_many :followed_users, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :followees, through: :followed_users, dependent: :destroy
+
+  has_many :following_users, class_name: "Follow", foreign_key: "followee_id", dependent: :destroy
+  has_many :followers, through: :following_users, dependent: :destroy
 end
