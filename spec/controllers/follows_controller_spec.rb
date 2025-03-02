97f5fa00e51ca9fe177bb94a1fd997797b995c34
Your Name <you@example.com>
2025-03-02 16:56:17 +0700

    Add follow controller spec for follow action

---
+++ spec/controllers/follows_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe FollowsController, type: :controller do
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+
+  describe "POST #follow" do
+    context "when the user is not already following the other user" do
+      it "successfully follows the other user" do
+        expect {
+          post :follow, params: { follower_id: user1.id, followee_id: user2.id }
+        }.to change(Follow, :count).by(1)
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when the user is already following the other user" do
+      before { create(:follow, follower: user1, followee: user2) }
+
+      it "returns an error" do
+        post :follow, params: { follower_id: user1.id, followee_id: user2.id }
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+  end
+end
