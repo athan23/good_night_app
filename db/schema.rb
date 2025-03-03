d86531874a06d642dbe5d8592bbe7467a1a95b07
Your Name <you@example.com>
2025-03-03 07:23:30 +0700

    create a migration and update db schema to add duration column in sleep records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_28_093404) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_03_002131) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id", null: false
     t.integer "followee_id", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_28_093404) do
     t.datetime "clock_out"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "duration"
     t.index ["user_id"], name: "index_sleep_records_on_user_id"
   end
 
