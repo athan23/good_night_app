c6fac4464ecb5b72549979c4aa1bc80450f7855d
Your Name <you@example.com>
2025-03-03 07:25:31 +0700

    create a callback in sleep record model to calculate sleep duration before saving

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,11 +1,17 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
 
+  before_save :calculate_duration
+
   # Add a callback in SleepRecord to clear cache when a record is created or updated
   # This ensures cached data stays fresh when users log sleep or update records
   after_commit :clear_cache
 
   private
+
+  def calculate_duration
+    self.duration = clock_out && clock_in ? (clock_out - clock_in).to_i : 0
+  end
 
   def clear_cache
     Rails.cache.delete_matched("user_#{user_id}_sleep_records_page_*")
