221ab86254c6ac9b825f8d64b9d3179d19b19d10
Your Name <you@example.com>
2025-03-04 04:13:46 +0700

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Clear existing data
+User.destroy_all
+SleepRecord.destroy_all
+Follow.destroy_all
+
+# Create Users
+users = []
+10.times do |i|
+  users << User.create!(name: "User #{i + 1}")
+end
+
+# Create Follows
+users.each do |user|
+  followees = users.sample(rand(2..5)) - [user] # Ensure they don't follow themselves
+  followees.each { |followee| Follow.create!(follower: user, followee: followee) }
+end
+
+# Create Sleep Records
+users.each do |user|
+  rand(5..10).times do
+    clock_in = Faker::Time.between(from: 2.weeks.ago, to: Time.now)
+    duration = [4, 6, 7, 8, 9].sample * 3600 # Sleep duration in seconds (4-9 hours)
+    clock_out = clock_in + duration.seconds
+
+    SleepRecord.create!(
+      user: user,
+      clock_in: clock_in,
+      clock_out: clock_out,
+      duration: duration
+    )
+  end
+end
